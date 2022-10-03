415dbab343e911d454e56eda76534364491913c3
Stephen Byrne <stephen.byrne@distilled.ie>
2022-10-03 09:43:19 +0100

    add next router

--- apps/countries/templates/CountryPage/CountryPage.tsx
+++ apps/countries/templates/CountryPage/CountryPage.tsx
@@ -1,14 +1,16 @@
 import * as Styles from './styles';
+import { useRouter } from 'next/router';
 import { useCountryPage } from './useCountryPage/useCountryPage';
 
 import { CountryCard } from './organisms/CountryCard';
 
 function CountryPage({ countryData }: { countryData: any }) {
   const { data, isLoading } = useCountryPage(countryData[0].borders.join(','));
+  const router = useRouter();
 
   return (
     <div>
-      <button onClick={() => history.back()}>Back</button>
+      <button onClick={() => router.back()}>Back</button>
       <Styles.CountryData>
         <img src={countryData[0].coatOfArms?.png} height={200} width={200} />
         <h1>{countryData[0].name.common}</h1>
