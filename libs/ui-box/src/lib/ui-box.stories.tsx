c432fcc00001e0cee53e03e0602d3dc7dd731ed8
Stephen Byrne <stephen.byrne@distilled.ie>
2022-10-28 16:53:07 +0100

    add box component

--- libs/ui-box/src/lib/ui-box.stories.tsx
+++ libs/ui-box/src/lib/ui-box.stories.tsx
@@ -2,13 +2,73 @@ import { ComponentStory, ComponentMeta } from '@storybook/react';
 
 import { UiBox } from './ui-box';
 
+import styled from 'styled-components';
+import React from 'react';
+
+const Title = styled.div`
+  grid-column: span 2;
+  height: 24px;
+`;
+
+const Price = styled.div`
+  height: 24px;
+`;
+
+const Spacer = styled.div`
+  height: 52px;
+  grid-column: span 2;
+`;
+
+const Likes = styled.div`
+  height: 24px;
+  text-align: right;
+`;
+
 export default {
-  /* 👇 The title prop is optional.
-   * See https://storybook.js.org/docs/react/configure/overview#configure-story-loading
-   * to learn how to generate automatic titles
-   */
   title: 'libs/UiBox',
   component: UiBox,
 } as ComponentMeta<typeof UiBox>;
 
-export const Primary: ComponentStory<typeof UiBox> = () => <UiBox />;
+export const Primary: ComponentStory<typeof UiBox> = () => (
+  <UiBox>
+    <Title>Box</Title>
+    <Spacer />
+    <Price>€1</Price>
+    <Likes>Likes: 45</Likes>
+  </UiBox>
+);
+
+const SUiBox = styled(UiBox)`
+  grid-template-areas:
+    'title title'
+    'spacer spacer'
+    'price likes';
+  grid-template-rows: 1fr 52px 1fr;
+`;
+
+const Title2 = styled.div`
+  grid-area: title;
+`;
+
+const Price2 = styled.div`
+  grid-area: price;
+`;
+
+const Likes2 = styled.div`
+  text-align: right;
+  grid-area: likes;
+`;
+
+const Spacer2 = styled.div`
+  text-align: right;
+  grid-area: spacer;
+`;
+
+export const Secondary: ComponentStory<typeof UiBox> = () => (
+  <SUiBox>
+    <Title2>Box</Title2>
+    <Price2>€1</Price2>
+    <Spacer2 />
+    <Likes2>Likes: 45</Likes2>
+  </SUiBox>
+);
--- libs/ui-box/src/lib/ui-box.tsx
+++ libs/ui-box/src/lib/ui-box.tsx
@@ -3,16 +3,30 @@ import styled from 'styled-components';
 /* eslint-disable-next-line */
 export interface UiBoxProps {}
 
-const StyledUiBox = styled.div`
-  color: pink;
+const Container = styled.div`
+  border-radius: 4px;
+  background-color: #ffffff;
+  box-shadow: 0 1px 4px rgba(51, 51, 51, 0.24);
+  padding: 8px;
+  font-size: 16px;
+  line-height: 24px;
+
+  display: grid;
+  grid-gap: 4px;
+
+  @media screen and (min-width: 64em) {
+    padding: 16px;
+  }
 `;
 
-export function UiBox(props: UiBoxProps) {
-  return (
-    <StyledUiBox>
-      <h1>Welcome to UiBox!</h1>
-    </StyledUiBox>
-  );
+export function UiBox({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) {
+  return <Container className={className}>{children}</Container>;
 }
 
 export default UiBox;
